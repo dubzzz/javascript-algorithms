87329dd9b0215683018a346694cc2bf387fe32b4
Nicolas DUBIEN <github@dubien.org>
2018-07-21 01:32:33 +0200

    Add property based tests on knuthMorrisPratt

--- src/algorithms/string/knuth-morris-pratt/__test__/knuthMorrisPratt.test.js
+++ src/algorithms/string/knuth-morris-pratt/__test__/knuthMorrisPratt.test.js
@@ -1,3 +1,4 @@
+import * as fc from 'fast-check';
 import knuthMorrisPratt from '../knuthMorrisPratt';
 
 describe('knuthMorrisPratt', () => {
@@ -8,5 +9,27 @@ describe('knuthMorrisPratt', () => {
     expect(knuthMorrisPratt('abcxabcdabxabcdabcdabcy', 'abcdabca')).toBe(-1);
     expect(knuthMorrisPratt('abcxabcdabxaabcdabcabcdabcdabcy', 'abcdabca')).toBe(12);
     expect(knuthMorrisPratt('abcxabcdabxaabaabaaaabcdabcdabcy', 'aabaabaaa')).toBe(11);
+  });
+  
+  it('should find a valid word position [property]', () => {
+    fc.assert(fc.property(fc.fullUnicodeString(), fc.fullUnicodeString(), (s1, s2) => {
+      const position = knuthMorrisPratt(s1, s2);
+      return position === -1 || s1.slice(position, position + s2.length) === s2;
+    }));
+  });
+  
+  it('should find a match [property]', () => {
+    fc.assert(
+      fc.property(
+        fc.fullUnicodeString(), fc.fullUnicodeString(), fc.fullUnicodeString(),
+        (a, b, c) => {
+          // temporary workaround
+          //   as knuthMorrisPratt("", "") === -1
+          //   while knuthMorrisPratt("a", "a") === 0
+          fc.pre(b !== '');
+          return knuthMorrisPratt(a + b + c, b) !== -1;
+        }
+      )
+    );
   });
 });
