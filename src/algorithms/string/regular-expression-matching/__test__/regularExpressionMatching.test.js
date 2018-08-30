c73c3336f0448bd347a55385cd4c5d344c112920
Nicolas DUBIEN <github@dubien.org>
2018-08-30 09:19:36 +0200

    Add property based tests on regularExpressionMatching

--- src/algorithms/string/regular-expression-matching/__test__/regularExpressionMatching.test.js
+++ src/algorithms/string/regular-expression-matching/__test__/regularExpressionMatching.test.js
@@ -1,3 +1,4 @@
+import * as fc from 'fast-check';
 import regularExpressionMatching from '../regularExpressionMatching';
 
 describe('regularExpressionMatching', () => {
@@ -30,5 +31,58 @@ describe('regularExpressionMatching', () => {
     expect(regularExpressionMatching('ab', 'a*')).toBe(false);
     expect(regularExpressionMatching('abba', 'a*b*.c')).toBe(false);
     expect(regularExpressionMatching('abba', '.*c')).toBe(false);
+  });
+
+  // Property:
+  // for any text, pattern strings
+  // where pattern is a valid regex pattern
+  //   and text is built such that it matches pattern
+  // there should be a match
+  it('should match on any valid (text, pattern) pairs [property]', () => {
+    // Generator of valid characters for our regex
+    const oneCharacterArb = fc.constantFrom(..."abcdefghijklmnopqrstuvwxyz");
+    // Generator of valid pattern structures
+    const patternStructureArb = fc.array(fc.oneof(
+        fc.record({ // pattern: a, text: a
+          type: fc.constant('singleExact'),
+          value: oneCharacterArb
+        }),
+        fc.record({ // pattern: ., text: a
+          type: fc.constant('singleAny'),
+          value: oneCharacterArb
+        }),
+        fc.record({ // pattern: a*, text: aaa
+          type: fc.constant('multiExact'),
+          value: oneCharacterArb,
+          num: fc.nat(10)
+        }),
+        fc.record({ // pattern: .*, text: abc
+          type: fc.constant('multiAny'),
+          value: fc.stringOf(oneCharacterArb)
+        }),
+    ));
+    // Generator of {text: string, pattern: string}
+    // where text is built such that it matches pattern
+    const patternAndTextArb = patternStructureArb.map(conf => {
+        return {
+            text: conf.map(v => {
+                if (v.type === 'singleExact') return v.value;
+                if (v.type === 'singleAny') return v.value;
+                if (v.type === 'multiExact') return v.value.repeat(v.num);
+                if (v.type === 'multiAny') return v.value;
+            }).join(''),
+            pattern: conf.map(v => {
+                if (v.type === 'singleExact') return v.value;
+                if (v.type === 'singleAny') return '.';
+                if (v.type === 'multiExact') return v.value + '*';
+                if (v.type === 'multiAny') return '.*';
+            }).join('')
+        };
+    });
+    fc.assert(
+      fc.property(
+        patternAndTextArb,
+        s => regularExpressionMatching(s.text, s.pattern)
+    ));
   });
 });
